56d0b59e2a6a61611a1c4c50b631939db1b17551
Dave Cahill <dpcahill@gmail.com>
2014-07-21 14:45:06 +0900

    Serve random square board

--- gol.go
+++ gol.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"code.google.com/p/goprotobuf/proto"
+	"math/rand"
 )
 
 func CreateEmptySquareBoard(boardSize int32) *BoardState {
@@ -18,8 +19,24 @@ func CreateEmptySquareBoard(boardSize int32) *BoardState {
 	return boardState
 }
 
+func CreateRandomSquareBoard(boardSize int32) *BoardState {
+	boardState := &BoardState{}
+	for i := int32(0); i < boardSize; i++ {
+		rowState := &BoardState_RowState{};
+		for j := int32(0); j < boardSize; j++ {
+			if rand.Int() % 2 == 0 {
+				rowState.CellStates = append(rowState.CellStates, CellState_DEAD)
+			} else {
+				rowState.CellStates = append(rowState.CellStates, CellState_ALIVE)
+			}
+		}
+		boardState.RowStates = append(boardState.RowStates, rowState)
+	}
+	return boardState
+}
+
 func newHandler(w http.ResponseWriter, r *http.Request) {
-	boardState := CreateEmptySquareBoard(4)
+	boardState := CreateRandomSquareBoard(4)
 	data, _ := proto.Marshal(boardState)
 	w.Write(data)
 }
