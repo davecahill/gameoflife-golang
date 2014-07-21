6c2b2f5b06478585bab59446c616e74718ac72af
Dave Cahill <dpcahill@gmail.com>
2014-07-21 14:38:11 +0900

    Turn into web server, serve new blank board

--- gol.go
+++ gol.go
@@ -1,7 +1,12 @@
 package main
 
-func CreateEmptySquareBoard(boardSize int32) BoardState {
-	boardState := BoardState{}
+import (
+	"net/http"
+	"code.google.com/p/goprotobuf/proto"
+)
+
+func CreateEmptySquareBoard(boardSize int32) *BoardState {
+	boardState := &BoardState{}
 	for i := int32(0); i < boardSize; i++ {
 		rowState := &BoardState_RowState{};
 		for j := int32(0); j < boardSize; j++ {
@@ -13,8 +18,13 @@ func CreateEmptySquareBoard(boardSize int32) BoardState {
 	return boardState
 }
 
-func main() {
-	println("hi")
+func newHandler(w http.ResponseWriter, r *http.Request) {
 	boardState := CreateEmptySquareBoard(4)
-	println(len(boardState.RowStates))
+	data, _ := proto.Marshal(boardState)
+	w.Write(data)
+}
+
+func main() {
+	http.HandleFunc("/new/", newHandler)
+	http.ListenAndServe(":8080", nil)
 }
