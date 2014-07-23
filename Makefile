56bb8876057a500c7abbcdd9130580b694003784
Dave Cahill <dpcahill@gmail.com>
2014-07-23 19:40:07 +0900

    Switch to JSON

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	protoc --go_out=. *.proto && go install dave-cahill.com/golserver
+	go install dave-cahill.com/golserver
--- gol.go
+++ gol.go
@@ -2,43 +2,61 @@ package main
 
 import (
 	"net/http"
-	"code.google.com/p/goprotobuf/proto"
 	"math/rand"
 	"io/ioutil"
+    "encoding/json"
+	"log"
 )
 
+// Define board and cells
+type cellState bool
+
+const Alive cellState = true
+const Dead cellState = true
+
+type BoardState struct {
+	States [][]cellState
+}
+
+// Board creation
+
+type CellChooser func() cellState
+
 func CreateEmptySquareBoard(boardSize int32) *BoardState {
-	boardState := &BoardState{}
-	for i := int32(0); i < boardSize; i++ {
-		rowState := &BoardState_RowState{};
-		for j := int32(0); j < boardSize; j++ {
-			cellState := CellState_DEAD
-			rowState.CellStates = append(rowState.CellStates, cellState)
-		}
-		boardState.RowStates = append(boardState.RowStates, rowState)
-	}
-	return boardState
+	deadCellChooser := CellChooser(func() cellState {
+		return Dead
+	})
+	return createSquareBoard(boardSize, deadCellChooser)
 }
 
 func CreateRandomSquareBoard(boardSize int32) *BoardState {
+	randomCellChooser := CellChooser(func() cellState {
+		return rand.Int() % 2 == 0
+	})
+	return createSquareBoard(boardSize, randomCellChooser)
+}
+
+func createSquareBoard(boardSize int32, cellChooser CellChooser) *BoardState {
 	boardState := &BoardState{}
+
 	for i := int32(0); i < boardSize; i++ {
-		rowState := &BoardState_RowState{};
+		rowState := []cellState{}
 		for j := int32(0); j < boardSize; j++ {
-			if rand.Int() % 2 == 0 {
-				rowState.CellStates = append(rowState.CellStates, CellState_DEAD)
-			} else {
-				rowState.CellStates = append(rowState.CellStates, CellState_ALIVE)
-			}
+			rowState = append(rowState, cellChooser())
 		}
-		boardState.RowStates = append(boardState.RowStates, rowState)
+		boardState.States = append(boardState.States, rowState)
 	}
 	return boardState
 }
 
+// Web server
+
 func newHandler(w http.ResponseWriter, r *http.Request) {
 	boardState := CreateRandomSquareBoard(4)
-	data, _ := proto.Marshal(boardState)
+	data, err := json.Marshal(boardState)
+	if err != nil {
+		log.Fatal(err)
+	}
 	w.Write(data)
 }
 
@@ -46,8 +64,6 @@ func baseHandler(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadFile("index.html")
 	w.Write(body)
 }
-
-
 
 func main() {
 	http.HandleFunc("/new/", newHandler)
